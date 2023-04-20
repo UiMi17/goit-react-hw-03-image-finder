bc4457f3eabeaf2a646358797ec59f2f22dd0ea7
Dan Dudaiev <roffrooff@gmail.com>
2023-04-20 16:54:07 +0300

    project init

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,5 @@
+export const Loader = (props) => {
+    return (
+        props
+    )
+}
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,9 @@
+export const Modal = props => {
+  return (
+    <div class="overlay">
+      <div class="modal">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
